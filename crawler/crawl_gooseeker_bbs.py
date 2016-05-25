f14549c2c88e012519bf30ef22ab2ba006f408dc
gz51837844 <2486516421@qq.com>
2016-05-25 23:16:44 +0800

    Upload craw_gooseeker_bbs.py , xslt_bbs.xml

---
+++ crawler/crawl_gooseeker_bbs.py
@@ -0,0 +1,21 @@
+#-*_coding:utf8-*-
+# 使用gsExtractor类的示例程序
+# 访问集搜客论坛，以xslt为模板提取论坛内容
+# xslt保存在xslt_bbs.xml中
+from urllib import request
+from lxml import etree
+from gooseeker import gsExtractor
+
+# 访问并读取网页内容
+url = "http://www.gooseeker.com/cn/forum/7"
+conn = request.urlopen(url)
+doc = etree.HTML(conn.read())
+
+# 生成xsltExtractor对象
+bbsExtra = gsExtractor()
+# 调用set方法设置xslt内容
+bbsExtra.setXsltFromFile("xslt_bbs.xml")
+# 调用extract方法提取所需内容
+result = bbsExtra.extract(doc)
+# 显示提取结果
+print(str(result))
