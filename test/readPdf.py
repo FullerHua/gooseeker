7b9f53b0eb9812a046c3ffdac51af56874854dd2
gz51837844 <2486516421@qq.com>
2016-07-19 10:58:45 +0800

    上载readPdf.py

---
+++ test/readPdf.py
@@ -0,0 +1,28 @@
+# _*_coding:utf8_*_
+# readPdf.py
+# python读取pdf格式的文档
+
+from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+
+def readPDF(pdfFile):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+
+    process_pdf(rsrcmgr, device, pdfFile)
+    device.close()
+
+    content = retstr.getvalue()
+    retstr.close()
+    return content
+
+pdfFile = urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf")
+outputString = readPDF(pdfFile)
+print(outputString)
+pdfFile.close()
